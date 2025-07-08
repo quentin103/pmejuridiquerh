0e2b803f8b61570bf82a0a305511d4f24588a7ae
Quentin <koffi.kouamelan.yq@gmail.com>
2025-07-08 22:47:42 +0000

    update menu

--- resources/views/super-admin/sections/super-admin-menu.blade.php
+++ resources/views/super-admin/sections/super-admin-menu.blade.php
@@ -73,6 +73,22 @@
         </x-menu-item>
         @endif
 
+         
+        <x-menu-item icon="baselegal" text="Base légale" :link="route('baselegal.index')">
+            <x-slot name="iconPath">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-journal-text" viewBox="0 0 16 16">
+                    <path
+                        d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
+                    <path
+                        d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
+                    <path
+                        d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
+                </svg>
+            </x-slot>
+        </x-menu-item>
+
+
         @if($sidebarSuperadminPermissions['manage_superadmin_front_settings'] != 5 && $sidebarSuperadminPermissions['manage_superadmin_front_settings'] != 'none')
 
         <x-menu-item icon="front" :text="__('superadmin.menu.frontSettings')" :link="route('superadmin.front-settings.front_theme_settings')">
